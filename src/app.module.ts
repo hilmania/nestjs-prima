a19b1b8283cf4eff8782beba4354f1a183318dde
Hilman Ibnu <hia.develop@gmail.com>
2022-10-12 16:08:47 +0700

    [on progress] add module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule],
+  imports: [PrismaModule, ArticlesModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
