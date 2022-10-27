862cdef4e276392de38fd95254cffe7527d24fa8
Hilman Ibnu <hia.develop@gmail.com>
2022-10-27 10:19:22 +0700

    [update] add validation and transformation ParseInt on route

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, AuthModule, UsersModule],
+  imports: [PrismaModule, ArticlesModule, AuthModule, UsersModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
